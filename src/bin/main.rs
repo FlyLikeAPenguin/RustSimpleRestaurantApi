426aa94f2b7e12a77e11dd1fc62084ebac311603
Joe Thornton <joe.clifford.thornton@gmail.com>
2022-05-26 20:11:02 +0100

    Split main into multiple methods for better code separation

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -25,27 +25,35 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
 
+    let (status_line, filename) = handle_path(buffer);
+
+    let contents = fs::read_to_string(filename).unwrap();
+
+    let response: String = build_response_string(status_line.to_string(), contents);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
+
+fn build_response_string(status_line: String, contents: String) -> String {
+    format!(
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status_line,
+        contents.len(),
+        contents
+    )
+}
+
+fn handle_path(buffer: [u8; 1024]) -> (&'static str, &'static str) {
     let get = b"GET / HTTP/1.1\r\n";
     let sleep = b"GET /sleep HTTP/1.1\r\n";
 
-    let (status_line, filename) = if buffer.starts_with(get) {
+    if buffer.starts_with(get) {
         ("HTTP/1.1 200 OK", "index.html")
     } else if buffer.starts_with(sleep) {
         thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK", "index.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND", "404.html")
-    };
-
-    let contents = fs::read_to_string(filename).unwrap();
-
-    let response = format!(
-        "{}\r\nContent-Length: {}\r\n\r\n{}",
-        status_line,
-        contents.len(),
-        contents
-    );
-
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+    }
 }
